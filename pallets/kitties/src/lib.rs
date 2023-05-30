53ca7ad5070ac5e5fdc93631627accbb4210a445
liaoxin <qqliaoxin@qq.com>
2023-05-30 18:04:31 +0800

    kitties

---
+++ pallets/kitties/src/lib.rs
@@ -0,0 +1,94 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// <https://docs.substrate.io/reference/frame-pallets/>
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	pub type KittyId = u32;
+	#[derive(Encode,Decode,Clone,Copy,RuntimeDebug,PartialEq,Eq,Default,TypeInfo,MaxEncodeLen)]
+	pub struct Kitty(pub [u8; 16]);
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+
+	// The pallet's runtime storage items.
+	// https://docs.substrate.io/main-docs/build/runtime-storage/
+	#[pallet::storage]
+	#[pallet::getter(fn next_kitty_id)]
+	pub type NextKittyId<T> = StorageValue<_, KittyId,ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn kitties)]
+	pub type Kitties<T> = StorageValue<_, Blake2_128Concat,KittyId,Kitty>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn kitty_owner)]
+	pub type KittyOwner<T: Config> = StorageValue<_, Blake2_128Concat,KittyId,T::AccountId>;
+
+	// Pallets use events to inform users when important changes are made.
+	// https://docs.substrate.io/main-docs/build/events-errors/
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Event documentation should end with an array that provides descriptive names for event
+		/// parameters. [something, who]
+		KittyCreated { who: T::AccountId, kitty_id: KittyId, kitty: Kitty },
+	}
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Error names should be descriptive.
+		InvalidKittyId,
+	}
+
+	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+	// These functions materialize as "extrinsics", which are often compared to transactions.
+	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// An example dispatchable that takes a singles value as a parameter, writes the value to
+		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn create(origin: OriginFor<T>) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			let kitty_id = Self::get_next_id()?;
+			let kitty = Kitty(Default::default());
+
+			Kitties::<T>::insert(kitty_id,&kitty);
+			KittyOwner::<T>::insert(kitty_id,&who);
+
+			Self::deposit_event(Event::KittyCreated{who,kitty_id,kitty});
+
+			// Emit an event.
+			Self::deposit_event(Event::SomethingStored(something, who));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+	
+	}
+	impl<T: Config> Pallet<T>{
+		fn get_next_id() -> Result<KittyId,DispatchError>{
+			NextKittyId::<T>::try_mutate(|next_id| ->Result<KittyId,DispatchError>{
+				let current_id = *next_id;
+				*next_id = next_id.checked_add(1).ok_or::<DispatchError>(Error::<T>::InvalidKittyId.into())?;
+				Ok(current_id)
+			})
+		}
+	}
+}
